4b3dc43afa993cf44ef7abb0e105723d2d746f5f
Max Watkins <willmwatkins@gmail.com>
2019-06-02 18:27:16 -0600

    basic API endpoints

--- Application/API/src/routes/api.ts
+++ Application/API/src/routes/api.ts
@@ -1,6 +1,23 @@
 import { Router, Request, Response } from 'express';
 
 const router: Router = Router();
+
+router.use('/character/:id', (req: Request, res: Response) => {
+  var id = req.params.id;
+  if(id){
+    res.send({id: id, name: "Kwothe", player: "Max"});
+  }
+    res.send(new Error('Character not found'));
+});
+
+router.use('/verify/:id', (req: Request, res: Response) => {
+  var id = req.params.id;
+  if(id){
+    res.send(true);
+  }
+    res.send(false);
+});
+
 router.use('/', (req: Request, res: Response) => {
     console.log('in index');
       res.sendFile('basic.html', {root: __dirname+'/../../public'});
